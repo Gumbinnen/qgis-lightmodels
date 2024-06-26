9c6b1c1cd1ea7536cbe3b2cebfe07f1fd732fb27
Gumbinnen <personal.gusev@gmail.com>
2024-06-27 00:51:12 +0500

    Remove range update in create_graduated_symbol()

--- models/gravity_model/gravity_model.py
+++ models/gravity_model/gravity_model.py
@@ -44,8 +44,8 @@ class GravityModel(QObject):
         self.layer_event_handler = LayerEventHandler(parent=self, data_manager=self.data_manager)
         
     def connect_signals(self):
-        connect_once(self.ui_widget.ready, self.go)
-        connect_once(self.ui_widget.export_request, self.export)
+        connect_once(self.ui_widget.ready, lambda *data: self.go(*data))
+        connect_once(self.ui_widget.export_request, lambda *data: self.export(*data))
         connect_once(self.layer_event_handler.feature_selection, self.feature_selection)
 
     def export(self, data_path: str, save_path: str, output_format: str):
@@ -63,9 +63,8 @@ class GravityModel(QObject):
         self.iface.addDockWidget(Qt.RightDockWidgetArea, self.ui_widget)
         self.ui_widget.show()
     
-    def feature_selection(self): # TODO: LayerEventHandler could return selection_ids, so code could be optimized
+    def feature_selection(self, selection_ids):
         layer = self.iface.activeLayer()
-        selection_ids = layer.selectedFeatureIds()
         
         if len(selection_ids) <= 0:
             return
@@ -173,26 +172,30 @@ class GravityModel(QObject):
             return line_layer
         
         def get_point_coords(point):
-            return point.y(), point.x()
+            return point.x(), point.y()
 
-        def create_graduated_symbol(field_name: str, n_classes: int, min_size: int, max_size: int, color: QColor):
+        def create_graduated_symbol(field_name: str, layer: QgsVectorLayer, n_classes: int, min_size: int, max_size: int, color: QColor):
+            # Create a graduated symbol renderer
             graduated_symbol = QgsGraduatedSymbolRenderer(field_name)
-            graduated_symbol.updateClasses(layer_tc, QgsGraduatedSymbolRenderer.EqualInterval, n_classes)
+        
+            # Update classes using Equal Interval method
+            graduated_symbol.updateClasses(layer, QgsGraduatedSymbolRenderer.EqualInterval, n_classes)
+        
+            # Set the graduated method to size
             graduated_symbol.setGraduatedMethod(QgsGraduatedSymbolRenderer.GraduatedSize)
+        
+            # Set the symbol sizes
             graduated_symbol.setSymbolSizes(min_size, max_size)
+        
+            # Update range labels
             graduated_symbol.updateRangeLabels()
-            ranges = []
-            for lower, upper in graduated_symbol.ranges():
-                symbol = QgsSymbol.defaultSymbol(layer_tc.geometryType())
-                symbol.setColor(color)
-                ranges.append(QgsRendererRange(lower, upper, symbol, f'{lower} - {upper}'))
 
-            graduated_symbol.updateRanges(ranges)
             return graduated_symbol
 
         def create_line_feature(point1, point2, associated_ids):
             line_geom = QgsGeometry.fromPolyline([point1, point2])
-            line_feature = QgsFeature().setGeometry(line_geom)
+            line_feature = QgsFeature()
+            line_feature.setGeometry(line_geom)
             line_feature.setAttributes(associated_ids)
             return line_feature
 
@@ -202,6 +205,7 @@ class GravityModel(QObject):
             for error in self.config.errors:
                 self.log('Config Validation Error: ', error, level=Qgis.Critical)
             return
+        
         # Инстанс проекта
         project = QgsProject.instance()
 
@@ -250,7 +254,6 @@ class GravityModel(QObject):
 
         # Массив данных модели. Размер массива len(features) x len(tc_features)
         gm_data = []
-        
         # Для каждой пары точек делаем рассчет по формуле и записываем результат в массив gm_data
         # https://en.wikipedia.org/wiki/Huff_model
         def huff_numerator(attractiveness, distance, α, β):
@@ -277,8 +280,7 @@ class GravityModel(QObject):
             
             total_h = sum(huff_numerators)
             row = [f.id()] + [
-                round(h / total_h, 4)
-                if total_h > 0
+                round(h / total_h, 4) if total_h > 0
                 else 0
                 for h in huff_numerators
             ]
@@ -308,7 +310,9 @@ class GravityModel(QObject):
         # Задание стиля для слоя поставщиков
         graduated_symbol = create_graduated_symbol(
             WEIGHT_FIELD_NAME,
-            n_classes=5, min_size=4, max_size=10,
+            layer=layer_tc,
+            n_classes=5,
+            min_size=4, max_size=10,
             color=QColor("red"))
         layer_tc.setRenderer(graduated_symbol)
         layer_tc.triggerRepaint()
