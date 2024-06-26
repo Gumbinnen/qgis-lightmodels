f0e557ac9cd3cf5e36a4b0509eb5f30848ec7dcd
Gumbinnen <personal.gusev@gmail.com>
2024-06-27 00:48:09 +0500

    Use connect_once() instead of connect()

--- models/gravity_model/layer_event_handler.py
+++ models/gravity_model/layer_event_handler.py
@@ -16,23 +16,26 @@ class LayerEventHandler(QObject):
         self.data_manager: DataManager = data_manager
         self.log = partial(log_function, title=type(self).__name__, tab_name='LightModels')
         
-        connect_once(self.iface.currentLayerChanged, self.on_active_layer_changed)
+        connect_once(
+            self.iface.currentLayerChanged,
+            lambda new_layer: self.on_active_layer_changed(new_layer)
+        )
     
-    def on_active_layer_changed(self):
-        current_layer = self.iface.activeLayer()
+    def on_active_layer_changed(self, new_layer):
+        if new_layer is None:
+            return
         
         # Проверка, создан ли слой классом GravityModel
-        if not self.data_manager.is_gm_layer(current_layer):
+        if not self.data_manager.is_gm_layer(new_layer):
             return
         
         # Новый активный слой
         if self._previous_layer:
             disconnect_safe(self._previous_layer.selectionChanged, self.feature_selection)
-        self._previous_layer = current_layer
+        self._previous_layer = new_layer
         
         # Вешаем сигнал feature_selection на текущий активный слой
-        current_layer.selectionChanged.connect(
+        connect_once(
+            new_layer.selectionChanged,
             lambda f_ids: self.feature_selection.emit(f_ids)
         )
-        # TODO: Replace connect() with connect_once():
-        # connect_once(current_layer.selectionChanged, self.feature_selection.emit)
